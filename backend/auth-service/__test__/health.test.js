01fcada30c883c542e0ee5eb93eb73b44d3a17e1
Kenny Cisneros <maxpropvp2@gmail.com>
2026-07-06 21:22:15 -0500

    fix: correct express app import syntax in auth service test

--- backend/auth-service/__test__/health.test.js
+++ backend/auth-service/__test__/health.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { app } = require('../src/server'); 
+const app  = require('../src/server'); 
 
 describe('Healthcheck API', () => {
   it('should respond with status 200 and indicate service is UP', async () => {
